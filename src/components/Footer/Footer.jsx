e3e377d4e93db728515b303337668f637ede87a4
Franco Zeta <perufra@hotmail.com>
2024-07-22 18:22:51 -0500

    add styles to floating menu with CSS & Update Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,11 @@
-import { useCart } from '../../hooks/useCart'
-import useFilter from '../../hooks/useFilters'
 import './Footer.css'
 
 const Footer = () => {
-  const { filters } = useFilter()
-  const { cart } = useCart()
   return (
     <footer className='footer'>
-      {/* <h4>Prueba Tecnica React ⚛️</h4>
+      <h4>Prueba Tecnica React ⚛️</h4>
       <span>@francozeta</span>
-      <h5>Shopping Cart con useContext & useReducer</h5> */}
-      {/*       {
-        JSON.stringify(filters, null, 2)
-      } */}
-      {
-        JSON.stringify(cart, null, 2)
-      }
+      <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   )
 }
