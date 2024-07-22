eda9a88e23645456b7eba138773b38af0c440a6e
Franco Zeta <perufra@hotmail.com>
2024-07-22 10:07:24 -0500

    Updated Products component to display correct icon based on whether the product is already in the cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,28 +1,41 @@
 import './Products.css'
-import { AddToCartIcon } from '../Icons'
+import { AddToCartIcon, RemoveFromCartIcon } from '../Icons'
+import { useCart } from '../../hooks/useCart'
 
 const Products = ({ products }) => {
+  const { addToCart, cart } = useCart()
+
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
   return (
     <>
       <main className='products'>
         <ul>
-          {products.slice(0, 10).map(product => (
-            <li key={product.id}>
-              <img
-                src={product.thumbnail}
-                alt={product.title}
-              />
-              <div>
-                <strong>{product.title}</strong> - ${product.price}
-              </div>
+          {products.slice(0, 10).map(product => {
+            const isProductInCart = checkProductInCart(product)
+            return (
+              <li key={product.id}>
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                />
+                <div>
+                  <strong>{product.title}</strong> - ${product.price}
+                </div>
 
-              <div>
-                <button>
-                  <AddToCartIcon />
-                </button>
-              </div>
-            </li>
-          ))}
+                <div>
+                  <button onClick={() => addToCart(product)}>
+                    {
+                      isProductInCart
+                        ? <RemoveFromCartIcon />
+                        : <AddToCartIcon />
+                    }
+                  </button>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </main>
     </>
