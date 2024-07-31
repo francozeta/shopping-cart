4e850d6a018db4cd62fac61ad87df82ac96b3667
Franco Zeta <perufra@hotmail.com>
2024-07-31 18:15:51 -0500

    Implement cartReducer with actions for adding, removing, and clearing items in the cart
    Added localStorage sync for cart state

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,5 +1,5 @@
-export const cartInitialState = []
-/* export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || [] */
+/* export const cartInitialState = [] */
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || []
 
 export const CART_ACTIONS_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
@@ -7,40 +7,52 @@ export const CART_ACTIONS_TYPES = {
   CLEAR_CART: 'CLEAR_CART'
 }
 
-export const cartReducer = (state, action) => {
-  const { type: actionType, payload: actionPayload } = action
+// Update local storage on state change
+export const updateLocalStorage = state => {
+  window.localStorage.setItem('cart', JSON.stringify(state))
+}
 
-  // Create a new array to avoid mutating the original state
-  // Handle actions here based on the type
-  switch (actionType) {
-    case CART_ACTIONS_TYPES.ADD_TO_CART: {
-      const { id } = actionPayload
-      const productInCartIndex = state.findIndex(item => item.id === id)
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTIONS_TYPES.ADD_TO_CART]: (state, action) => {
+    // Create a new array to avoid mutating the original state
+    // Handle actions here based on the type
+    const { id } = action.payload
+    const productInCartIndex = state.findIndex(item => item.id === id)
 
-      if (productInCartIndex >= 0) {
-        // one way would be use Structured Clone to make a deep copy of the cart array
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        return newState
-      }
-      // If the product is not in the cart
-      return [
-        ...state,
-        {
-          ...actionPayload,
-          quantity: 1
-        }
+    if (productInCartIndex >= 0) {
+      // one way would be use Structured Clone to make a deep copy of the cart array
+      const newState = [
+        ...state.slice(0, productInCartIndex),
+        { ...state[productInCartIndex], quantity: state[productInCartIndex].quantity + 1 },
+        state.slice(productInCartIndex + 1)
       ]
+      updateLocalStorage(newState)
+      return newState
     }
-
-    case CART_ACTIONS_TYPES.REMOVE_FROM_CART: {
-      const { id } = actionPayload
-      return state.filter(item => item.id !== id)
-    }
-
-    case CART_ACTIONS_TYPES.CLEAR_CART: {
-      return cartInitialState
-    }
+    const newState = [
+      ...state,
+      {
+        ...action.payload, // product
+        quantity: 1
+      }
+    ]
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_ACTIONS_TYPES.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = action.payload
+    const newState = state.filter(item => item.id !== id)
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_ACTIONS_TYPES.CLEAR_CART]: () => {
+    updateLocalStorage([])
+    return []
   }
-  return state
+}
+
+export const cartReducer = (state, action) => {
+  const { type: actionType } = action
+  const updateState = UPDATE_STATE_BY_ACTION[actionType]
+  return updateState ? updateState(state, action) : state
 }
